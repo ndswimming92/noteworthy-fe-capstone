4a04505b2f3bcff0df5a0311758d2c66c3b8d4dd
Nicholas Davidson <nicholas.davidson.20@gmail.com>
2023-11-11 13:19:24 -0600

    updated data

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 // import { Button } from 'react-bootstrap'; // TODO: COMMENT IN FOR AUTH
 // import { signOut } from '../utils/auth'; // TODO: COMMENT IN FOR AUTH
-// import { useAuth } from '../utils/context/authContext'; // TODO: COMMENT IN FOR AUTH
+import { useAuth } from '../utils/context/authContext'; // TODO: COMMENT IN FOR AUTH
 
 function Home() {
-  // const { user } = useAuth(); // TODO: COMMENT IN FOR AUTH
+  const { user } = useAuth(); // TODO: COMMENT IN FOR AUTH
 
-  const user = { displayName: 'Dr. T' }; // TODO: COMMENT OUT FOR AUTH
+  // const user = { displayName: 'Dr. T' }; // TODO: COMMENT OUT FOR AUTH
   return (
     <div
       className="text-center d-flex flex-column justify-content-center align-content-center"
