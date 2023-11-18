fe0b9fa80cb3cdc83d6b2b69c2ed63960cf14945
Nicholas Davidson <nicholas.davidson.20@gmail.com>
2023-11-18 13:45:08 -0600

    deleted some files not needed and updated login button color

--- components/Signin.js
+++ components/Signin.js
@@ -15,7 +15,7 @@ function Signin() {
     >
       <h1>Hi there!</h1>
       <p>Click the button below to login!</p>
-      <Button type="button" size="lg" className="copy-btn" onClick={signIn}>
+      <Button type="button" size="lg" className="btn btn-success" onClick={signIn}>
         Sign In
       </Button>
     </div>
