fe850506f63e2764e2fee9e42eb0d35f392fe0b5
Nicholas Davidson <nicholas.davidson.20@gmail.com>
2023-11-20 19:40:25 -0600

    created all API and fetch calls.

--- components/NavBarAuth.js
+++ components/NavBarAuth.js
@@ -11,14 +11,17 @@ export default function NavBarAuth() {
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
         <Link passHref href="/">
-          <Navbar.Brand>CHANGE ME</Navbar.Brand>
+          <Navbar.Brand>Noteworthy</Navbar.Brand>
         </Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="ms-auto">
             {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
             <Link passHref href="/">
-              <Nav.Link>Home</Nav.Link>
+              <Nav.Link>Notes</Nav.Link>
+            </Link>
+            <Link passHref href="/">
+              <Nav.Link>Note+</Nav.Link>
             </Link>
             <Button variant="danger" onClick={signOut}>Sign Out</Button>
           </Nav>
